3baeda7d62955667211a4c3853a07576fb8e3f2b
Joao Junior <Joaodanieljr@gmail.com>
2021-03-25 20:19:52 -0300

    day one

---
+++ day1.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+var str = "Hello Playground"
+str = "Good Bye"
+
+var age = 27
+var population = 20_000_000
+
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+var str2 = """
+this goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+
+var awesome = true 
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+let language = "swift"
+
+let year:Int = 1993
+let name:String = "Joao"
